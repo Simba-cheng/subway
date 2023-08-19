8592f462cfba128f2e82a918e789f406f37283af
Cuvii <xw5427@gmail.com>
2023-08-19 19:16:46 +0800

    feat: draw lines

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,26 @@
 import { MapboxMap } from '@studiometa/vue-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { type LayersList } from '@deck.gl/core/typed'
-import hexRGB from 'hex-rgb'
-import subway from '~/datasource/subway.json'
+import hexRgb from 'hex-rgb'
+import subway from '~/datasource/data.json'
 
 const config = useRuntimeConfig()
 const map = ref()
 const mapCenter = ref([113.863048, 22.575149])
 
 watch(map, async (map) => {
-  const { ScatterplotLayer } = await import('@deck.gl/layers/typed')
+  const { ScatterplotLayer, PathLayer } = await import('@deck.gl/layers/typed')
   const DeckOverlay = await import('@deck.gl/mapbox/typed').then(module => module.MapboxOverlay)
 
   const layers: LayersList[] = []
   subway.forEach((city) => {
     const cityLayer: LayersList = []
     city.lines.forEach((line) => {
-      const lineColor = hexRGB(line.color, { format: 'array', alpha: 255 })
+      const lineColor = hexRgb(line.color, { format: 'array', alpha: 255 })
 
       cityLayer.push(new ScatterplotLayer({
         id: `${city.cityname}${line.name} stations`,
         data: line.stations,
-        pickable: true,
         opacity: 0.8,
         stroked: true,
         filled: true,
@@ -33,10 +32,27 @@ watch(map, async (map) => {
         radiusMaxPixels: 6,
         lineWidthMinPixels: 1,
         getPosition(data) {
-          return transformGCJ02(transformStringToGCJ02(data.coord))
+          return transformGCJ02([data.lng, data.lat])
         },
         getFillColor: lineColor,
       }))
+
+      if (line.polyline?.length) {
+        const polylineData = extractPolyline(line.polyline)
+        cityLayer.push(new PathLayer<[number, number][]>({
+          id: `${city.cityname}${line.name} polyline`,
+          data: polylineData,
+
+          getPath: d => polylineData,
+          getColor: d => lineColor,
+          getWidth: d => 5,
+          pickable: false,
+          autoHighlight: false,
+          widthScale: 15,
+          widthMinPixels: 2,
+          widthMaxPixels: 3,
+        }))
+      }
     })
 
     layers.push(cityLayer)
--- utils/coord.ts
+++ utils/coord.ts
@@ -1,4 +1,5 @@
 import gcoord from 'gcoord'
+import chunk from 'lodash-es/chunk'
 
 type LAT = number
 type LNG = number
@@ -7,6 +8,10 @@ export function transformGCJ02(raw: [LAT, LNG]) {
   return gcoord.transform(raw, gcoord.GCJ02, gcoord.WGS84)
 }
 
-export function transformStringToGCJ02(str: string) {
-  return str.split(',').reverse().map(Number) as [LAT, LNG]
+export function extractPolyline(polyline: number[]) {
+  const coords: number[] = [...polyline]
+  for (let i = 2; i < coords.length; i++)
+    coords[i] = coords[i - 2] + coords[i] / 1000000
+
+  return chunk(coords, 2).map(a => transformGCJ02(a.reverse() as [number, number]))
 }
