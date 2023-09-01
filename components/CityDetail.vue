cb0d6a3498f12ef31a292fdeca8bc61136ea7ee3
Cuvii <xw5427@gmail.com>
2023-09-01 19:42:44 +0800

    feat: animate panels

--- components/CityDetail.vue
+++ components/CityDetail.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { X } from 'lucide-vue-next'
+import { Motion, Presence } from 'motion/vue'
 import { useAppStore } from '~/store/app.store'
 import type { Line } from '~/types'
 import { useInteractorStore } from '~/store/interactor.store'
@@ -16,23 +17,31 @@ const { setFocusedLine, setHoveringLine } = useInteractorStore()
 </script>
 
 <template>
-  <div v-if="detailCity" class="fixed left-28 top-4 bg-white text-xs p-2 rounded-lg">
-    <h3 class="flex items-center">
-      <span>{{ detailCity.name }}</span>
-      <X class="ml-auto cursor-pointer hover:scale-110" :size="12" @click="() => emits('close')" />
-    </h3>
-    <ul class="text-zinc-600 space-y-1 mt-2">
-      <li
-        v-for="line in detailCity.lines" :key="line.id" class="relative" @click="() => {
-          emits('line-select', line)
-          setFocusedLine(line)
-        }" @mouseenter="() => setHoveringLine(line)" @mouseleave="() => setHoveringLine(null)"
-      >
-        <div class="hover:bg-zinc-50 rounded px-2 py-1 flex items-center cursor-pointer">
-          <div class="h-1 w-1 rounded-full mr-1" :style="{ background: `rgb(${line.color.slice(0, 3)})` }" />
-          {{ line.name }}
-        </div>
-      </li>
-    </ul>
-  </div>
+  <Presence>
+    <Motion
+      v-if="detailCity"
+      :initial="{ opacity: 0.95, y: -50 }"
+      :animate="{ opacity: 1, y: 0 }"
+      :exit="{ opacity: 0, y: -50 }"
+      class="fixed left-28 top-4 bg-white text-xs p-2 rounded-lg"
+    >
+      <h3 class="flex items-center">
+        <span>{{ detailCity.name }}</span>
+        <X class="ml-auto cursor-pointer hover:scale-110" :size="12" @click="() => emits('close')" />
+      </h3>
+      <ul class="text-zinc-600 space-y-1 mt-2">
+        <li
+          v-for="line in detailCity.lines" :key="line.id" class="relative" @click="() => {
+            emits('line-select', line)
+            setFocusedLine(line)
+          }" @mouseenter="() => setHoveringLine(line)" @mouseleave="() => setHoveringLine(null)"
+        >
+          <div class="hover:bg-zinc-50 rounded px-2 py-1 flex items-center cursor-pointer">
+            <div class="h-1 w-1 rounded-full mr-1" :style="{ background: `rgb(${line.color.slice(0, 3)})` }" />
+            {{ line.name }}
+          </div>
+        </li>
+      </ul>
+    </Motion>
+  </Presence>
 </template>
--- components/LineDetail.vue
+++ components/LineDetail.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import { Motion, Presence } from 'motion/vue'
 import { useInteractorStore } from '~/store/interactor.store'
 import type { Station } from '~/types'
 
@@ -11,23 +12,29 @@ const { focusedLine } = storeToRefs(useInteractorStore())
 </script>
 
 <template>
-  <div v-if="!!focusedLine" class="fixed right-4 top-4 backdrop-blur-lg bg-white py-4 pb-0 rounded-lg min-w-[70px]">
-    <h3 class="text-center text-xs text-zinc-700">
-      {{ focusedLine.name }}
-    </h3>
-    <div class="max-h-[80vh] overflow-y-scroll scrollbar-none">
-      <ul class="mt-3 text-xs text-stone-500 my-2 px-1">
-        <li
-          v-for="(station, idx) in focusedLine.stations" :key="station.id" class="hover:bg-zinc-100 rounded p-1.5 cursor-pointer" @click="() => {
-            emits('station-click', station)
-          }"
-        >
-          <span class="inline-block w-2 text-right mr-2" :style="{ color: `rgb(${focusedLine.color.slice(0, 3)})` }">
-            {{ idx + 1 }}
-          </span>
-          {{ station.name }}
-        </li>
-      </ul>
-    </div>
-  </div>
+  <Presence>
+    <Motion
+      v-if="!!focusedLine" :initial="{ opacity: 0, x: 50 }" :animate="{ opacity: 1, x: 0 }" :exit="{ opacity: 0, x: 50 }"
+      class="fixed right-4 top-4 backdrop-blur-lg bg-white py-4 pb-0 rounded-lg min-w-[70px]"
+    >
+      <h3 class="text-center text-xs text-zinc-700">
+        {{ focusedLine.name }}
+      </h3>
+      <div class="max-h-[80vh] overflow-y-scroll scrollbar-none">
+        <ul class="mt-3 text-xs text-stone-500 my-2 px-1">
+          <li
+            v-for="(station, idx) in focusedLine.stations" :key="station.id"
+            class="hover:bg-zinc-100 rounded p-1.5 cursor-pointer" @click="() => {
+              emits('station-click', station)
+            }"
+          >
+            <span class="inline-block w-2 text-right mr-2" :style="{ color: `rgb(${focusedLine.color.slice(0, 3)})` }">
+              {{ idx + 1 }}
+            </span>
+            {{ station.name }}
+          </li>
+        </ul>
+      </div>
+    </Motion>
+  </Presence>
 </template>
