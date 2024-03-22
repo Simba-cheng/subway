5981f40de80e4ad090dbaf117c50badf8f7f0755
Cuvii <i@cuvii.dev>
2024-03-22 11:42:24 +0800

    feat: better sidebar

--- components/CitySelect.vue
+++ components/CitySelect.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { ChevronUp, List, X } from 'lucide-vue-next'
+import { List, X } from 'lucide-vue-next'
 import { useAppStore } from '~/store/app.store'
 import type { City } from '~/types'
 
@@ -23,47 +23,37 @@ watchEffect(() => {
 
 <template>
   <div class=" bg-white rounded text-zinc-800 text-xs p-2 pb-2 flex flex-col flex-1 max-h-full">
-    <ul v-if="selectedCities.size">
-      <li
-        v-for="city in selectedCities" :key="city.id" class="hover:bg-zinc-100 rounded py-1 px-1 cursor-pointer flex" @click="() => {
-          emits('on-zoom', city)
-        }"
-      >
-        <span class="mr-2 select-none">
-          {{ city.name }}
-        </span>
-        <span class="flex ml-auto space-x-1 items-center">
-          <span
-            class="p-0.5 cursor-pointer" @click.stop="() => {
-              setDetailCity(city)
-            }"
-          >
-            <List class="hover:scale-125" :size="12" />
-          </span>
-          <span
-            class="cursor-pointer" @click.stop="() => {
-              deselectCity(city)
-              emits('city-unselect', city)
-            }"
-          >
-            <X class="text-red-500 hover:scale-125 transition-transform" :size="12" />
-          </span>
-        </span>
-      </li>
-    </ul>
-    <div v-if="selectedCities.size" class="my-1 flex justify-center">
-      <ChevronUp :size="14" class="transition-transform cursor-pointer" :class="!isOpen && 'rotate-180'" @click="() => isOpen = !isOpen" />
-    </div>
     <section class="grow overflow-scroll scrollbar-none" :class="!isOpen && `max-h-0`">
       <ul class="select-none">
         <li
-          v-for="city in dataset.filter(city => !isCitySelected(city))" :key="city.id" class="group text-xs text-zinc-400 p-1 flex items-center cursor-pointer hover:bg-zinc-100 rounded hover:text-zinc-500"
+          v-for="city in dataset" :key="city.id"
+          class="group text-xs text-zinc-400 p-1 flex items-center cursor-pointer hover:bg-zinc-100 rounded hover:text-zinc-500"
           @click="() => {
             selectCity(city)
             emits('city-select', city)
           }"
         >
-          {{ city.name }}
+          <span>
+
+            {{ city.name }}
+          </span>
+          <span class="flex ml-auto space-x-1 items-center opacity-0 pointer-events-none" :class="isCitySelected(city) && 'opacity-100 pointer-events-auto'">
+            <span
+              class="p-0.5 cursor-pointer" @click.stop="() => {
+                setDetailCity(city)
+              }"
+            >
+              <List class="hover:scale-125" :size="12" />
+            </span>
+            <span
+              class="cursor-pointer" @click.stop="() => {
+                deselectCity(city)
+                emits('city-unselect', city)
+              }"
+            >
+              <X class="text-red-500 hover:scale-125 transition-transform" :size="12" />
+            </span>
+          </span>
         </li>
       </ul>
     </section>
